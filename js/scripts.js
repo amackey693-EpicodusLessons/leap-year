26eb4e5796bfd6b50a1e31a56562308318d5b1f4
Allison Mackey <a.mackey693@gmail.com>
2020-04-01 09:33:47 -0700

    spec for provided year is divisible by 4 returns true, spec passes
    co-authored-by: Spencer Moody <spencer.moody@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,20 @@
 // Business Logic Here: 
 
 var leapYear = function(year) {
-  return false;
-}
+  if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 
 // User Interface Logic Here: 
 $(document).ready(function(){
   $("form#leap-year").submit(function(event){
-  var year = parseInt($("input#year").val());
-  var result = leapYear(year);
-  $("#result").text(result);
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
   });
 });
